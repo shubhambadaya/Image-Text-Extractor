0ea9e98a9478d59ca34cb0e1d5809f3c2f9d8dfc
shubhambadaya <86912972+shubhambadaya@users.noreply.github.com>
2021-09-05 11:39:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import base64
+import json
 import os
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/jupyter/image-classification-mlops-e48685e1fcab.json"
 
 from google.cloud import vision_v1
 
+# [START functions_ocr_process]
+def process_image(file, context):
+    """Cloud Function triggered by Cloud Storage when a file is changed.
+    Args:
+        file (dict): Metadata of the changed file, provided by the triggering
+                                 Cloud Storage event.
+        context (google.cloud.functions.Context): Metadata of triggering event.
+    Returns:
+        None; the output is written to stdout and Stackdriver Logging
+    """
+    bucket = validate_message(file, "bucket")
+    name = validate_message(file, "name")
 
-def sample_batch_annotate_files(storage_uri):
+    detect_text(bucket, name)
+
+    print("File {} processed.".format(file["name"]))
+
+
+def sample_batch_annotate_files(bucket,filename):
 
     text = ""
     """Perform batch file annotation."""
@@ -12,7 +32,7 @@ def sample_batch_annotate_files(storage_uri):
 
     client = vision_v1.ImageAnnotatorClient()
 
-    gcs_source = {"uri": storage_uri}
+    gcs_source = {"uri": f"gs://{bucket}/{filename}"}
     input_config = {"gcs_source": gcs_source, "mime_type": mime_type}
     features = [{"type_": vision_v1.Feature.Type.DOCUMENT_TEXT_DETECTION}]
 
@@ -27,6 +47,6 @@ def sample_batch_annotate_files(storage_uri):
         print('=========================================')
         print(u"Full text: {}".format(image_response.full_text_annotation.text))
 
-        text += image_response.full_text_annotation.text
+        #text += image_response.full_text_annotation.text
 
-    return(text)
+    #return(text)
